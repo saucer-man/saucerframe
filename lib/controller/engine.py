6cd2dba0df35bfcf70aace87a3dd8b80ae41b842
saucerman <gyq0929@gmail.com>
2018-12-08 02:36:31 +0800

    Modify output of banner

--- lib/controller/engine.py
+++ lib/controller/engine.py
@@ -14,50 +14,53 @@ from lib.core.common import outputscreen
 from lib.core.enums import POC_RESULT_STATUS
 
 def initEngine():
+    # init control parameter
     th.result = []
-    th.thread_count = th.thread_num = conf.thread_num # 线程
-    th.module_name = conf.module_name #  test.py
-    th.module_path = conf.module_path #  D:\saucerframe\scripts\test.py
-    th.module_obj = conf.module_obj # POC
-    th.target = conf.target # target queue
-    th.no_output = conf.no_output # True /False
-    th.output_path = conf.output_path # D:\saucerframe\output\2018-12-02 22:30:26.txt
-    th.scan_count = th.found_count = 0  # 已经扫描数 + 已经找到数
-    th.is_continue = True # 继续？控制参数
-    th.start_time = time.time() # 开始时间 
-    setThreadLock() # 线程锁初始话，下面--》
-    msg = 'Set the number of concurrent: %d' % th.thread_num # 线程数
+    th.thread_count = th.thread_num = conf.thread_num
+    th.module_name = conf.module_name
+    th.module_path = conf.module_path 
+    th.module_obj = conf.module_obj 
+    th.target = conf.target
+    th.no_output = conf.no_output 
+    th.output_path = conf.output_path 
+    th.scan_count = th.found_count = 0 
+    th.is_continue = True 
+    th.start_time = time.time() 
+    setThreadLock() 
+    msg = '[+] Set the number of thread: %d' % th.thread_num 
     outputscreen.success(msg) 
 
-def setThreadLock(): # 生成锁对象，全局唯一
-    th.found_count_lock = threading.Lock() # 找到数目锁
-    th.scan_count_lock = threading.Lock() # 扫描数目锁
-    th.thread_count_lock = threading.Lock() # 线程过程锁
-    th.file_lock = threading.Lock() # 文件锁
-    th.load_lock = threading.Lock() # 加载锁
+def setThreadLock(): 
+    # set thread lock 
+    th.found_count_lock = threading.Lock()
+    th.scan_count_lock = threading.Lock()
+    th.thread_count_lock = threading.Lock()
+    th.file_lock = threading.Lock() 
+    th.load_lock = threading.Lock() 
 
 def scan():
     while True:
-        th.load_lock.acquire()# 则加载线程锁
-        if th.target.qsize() > 0 and th.is_continue: # 如果queue非空，继续
-            payload = str(th.target.get(timeout=1.0)) # pop 一个 payload
-            th.load_lock.release() # 释放线程锁
+        th.load_lock.acquire()
+        if th.target.qsize() > 0 and th.is_continue: 
+            payload = str(th.target.get(timeout=1.0)) 
+            th.load_lock.release() 
         else:
-            th.load_lock.release() # 释放线程锁
+            th.load_lock.release() 
             break
         try:
-            # POC在执行时报错如果不被处理，线程框架会停止并退出
-            status = th.module_obj.poc(payload) # payload加载进module
+            status = th.module_obj.poc(payload) 
             resultHandler(status, payload) 
         except Exception:
             th.errmsg = traceback.format_exc()
             th.is_continue = False
-        changeScanCount(1) # 已扫描的payload+1
-    changeThreadCount(-1) # 正在进行的进程数-1
+        # set scanned count + 1
+        changeScanCount(1) 
+    # set running thread -1
+    changeThreadCount(-1) # 
 
 def run():
-    initEngine() # th参数设置
-    for i in range(th.thread_num): # 创建多线程 
+    initEngine()
+    for i in range(th.thread_num): 
         t = threading.Thread(target=scan, name=str(i))
         t.setDaemon(True)
         t.start()
@@ -67,40 +70,48 @@ def run():
             time.sleep(0.01)
         else:
             break
-    if not th.no_output: # 写入文件
+    # save result to output file
+    if not th.no_output:
         output2file(th.result) 
     printProgress()
     if 'errmsg' in th:
         outputscreen.error(th.errmsg)
 
 def resultHandler(status, payload):
+    # if no vulnerable
     if not status or status is POC_RESULT_STATUS.FAIL:
-        msg = '[-] ' + payload
-        outputscreen.success(msg) # 没找到
+        # msg = '[-] ' + payload
+        # outputscreen.info(msg)
         return 
+    # try again 
     elif status is POC_RESULT_STATUS.RETRAY:
-        changeScanCount(-1) # 如果返回status是2，则将已扫描数-1；
-        th.target.put(payload) # 并且payload重新put进queue
+        changeScanCount(-1) 
+        th.target.put(payload) 
         return
+    # vulnerable
     elif status is True or status is POC_RESULT_STATUS.SUCCESS:
         msg = '[+] ' + payload
-        outputscreen.success(msg) # 成功了
+        outputscreen.info(msg) # 成功了
     else:
         msg = str(status)
         outputscreen.warning(msg)
-    changeFoundCount(1) # 成功找的的payload加1
+    # get found number of payload +1
+    changeFoundCount(1) 
+
     th.result.append(msg)
-    if len(th.result) > 1000:
-        if not th.no_output: # 写入文件
+
+    # if result list is too large, save it to file and empty list
+    if len(th.result) > 5000:
+        if not th.no_output:
             output2file(th.result) 
             th.result = []
 
-def changeScanCount(num): # 改变已经扫描的payload数+num 
+def changeScanCount(num): 
     th.scan_count_lock.acquire()
     th.scan_count += num
     th.scan_count_lock.release()
 
-def output2file(msg): # 将msg列表写入文件
+def output2file(msg): 
     th.file_lock.acquire()
     f = open(th.output_path, 'a')
     for res in msg:
@@ -108,18 +119,18 @@ def output2file(msg): # 将msg列表写入文件
     f.close()
     th.file_lock.release()
 
-def changeFoundCount(num): # 改变找到目标的数目
+def changeFoundCount(num):
     th.found_count_lock.acquire()
     th.found_count += num
     th.found_count_lock.release()
 
-def changeThreadCount(num): # 改变正在扫描的线程数
+def changeThreadCount(num): 
     th.thread_count_lock.acquire()
     th.thread_count += num
     th.thread_count_lock.release()
 
-def printProgress(): # 输出状态
+def printProgress(): 
     msg = '%s found | %s remaining | %s scanned in %.2f seconds' % (
         th.found_count, th.target.qsize(), th.scan_count, time.time() - th.start_time)
     # out = '\r' + ' ' * (th.console_width - len(msg)) + msg
-    outputscreen.info(msg)
+    outputscreen.blue(msg)
--- saucerframe.py
+++ saucerframe.py
@@ -8,7 +8,7 @@ See the file 'LICENSE' for copying permission
 
 import os
 from lib.parse.cmdline import cmdLineParser
-from lib.core.common import outputscreen, setpaths
+from lib.core.common import outputscreen, setpaths, banner
 from lib.core.data import paths, conf, cmdLineOptions
 from lib.core.option import initOptions
 from lib.controller.engine import run
@@ -17,17 +17,19 @@ def main():
     """
     main fuction of saucerframe 
     """
-    paths.ROOT_PATH = os.getcwd() # 获取root_path D:\saucerframe
-    setpaths() # 设置路径
-    cmdLineOptions.update(cmdLineParser().__dict__) # cmdLineOptions接收参数
-    #{'thread_num': 10, 'script_name': '', 'target_single': 'fdsafd', 'target_file': '', 'output_file': '', 'output_file_status': True}
-    outputscreen.banner() # 输出脚本
-
+    # set paths of project 
+    paths.ROOT_PATH = os.getcwd()
+    setpaths()
     
-    initOptions(cmdLineOptions) # 设置了一下参数给conf
+    # received command >> cmdLineOptions
+    cmdLineOptions.update(cmdLineParser().__dict__)
+    
+    # loader script,target,threads,output_file from cmdLineOptions
+    # and send it to conf
+    initOptions(cmdLineOptions) 
 
-    run() # 运行engine
-
+    # run!
+    run()
 
 if __name__ == "__main__":
     main()
