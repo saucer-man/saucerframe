ca272ebe1566a73fa3636b44f8abcfc4098e7ed6
saucerman <gyq0929@gmail.com>
2018-12-08 01:10:17 +0800

    fix color output

--- lib/core/option.py
+++ lib/core/option.py
@@ -34,16 +34,17 @@ def checkShow(args):
     if args.show_scripts:
         module_name_list = os.listdir(paths.SCRIPT_PATH)
         #print(module_name_list)
-        outputscreen.info('Script Name (total:%s)\n' % str(len(module_name_list) - 1))
+        outputscreen.success('[+] Script Name (total:%s)\n' % str(len(module_name_list) - 1))
+        order = 1
         for module in module_name_list:
-            outputscreen.info(module)
+            outputscreen.success(str(order)+ '. ' +module)
         sys.exit()
 
 
 def EngineRegister(args):
     if args.thread_num > 100 or args.thread_num < 1:
-        # msg = 'Invalid input in [-t], range: 1 to 100, has changed to default(10)'
-        # outputscreen.error(msg)
+        msg = '[*] Invalid input in [-t](range: 1 to 100), has changed to default(10)'
+        outputscreen.warning(msg)
         conf.thread_num = 10
         return 
     conf.thread_num = args.thread_num
@@ -52,7 +53,7 @@ def ScriptRegister(args):
 
     # 如果指定脚本
     if not args.script_name:
-        msg = 'Use -s to load script. Example: [-s spider] or [-s ./script/spider.py]'
+        msg = '[-] Use -s to load script. Example: [-s spider] or [-s ./script/spider.py]'
         outputscreen.error(msg)
         sys.exit()
 
@@ -64,15 +65,15 @@ def ScriptRegister(args):
                     conf.module_name = os.path.split(args.script_name)[-1]
                     conf.module_path = os.path.abspath(args.script_name)
                 else:
-                    msg = '[%s] not a Python file. Example: [-s spider] or [-s ./scripts/spider.py]' % args.script_name
-                    outputscreen.error(msg)
+                    msg = '[-] [%s] not a Python file. Example: [-s spider] or [-s ./scripts/spider.py]' % args.script_name
+                    outputscreen.error('[-] ' +msg)
                     sys.exit()
             else:
-                msg = '[%s] not a file. Example: [-s spider] or [-s ./scripts/spider.py]' % args.script_name
+                msg = '[-] [%s] not a file. Example: [-s spider] or [-s ./scripts/spider.py]' % args.script_name
                 outputscreen.error(msg)
                 sys.exit()
         else:
-            msg = '[%s] not found. Example: [-s spider] or [-s ./scripts/spider.py]' % args.script_name
+            msg = '[-] [%s] not found. Example: [-s spider] or [-s ./scripts/spider.py]' % args.script_name
             outputscreen.error(msg)
             sys.exit()
 
@@ -85,54 +86,45 @@ def ScriptRegister(args):
             conf.module_name = args.script_name
             conf.module_path = os.path.abspath(_path)
         else:
-            msg = 'Script [%s] not exist. Use [--show] to view all available script in ./scripts/' % args.script_name
+            msg = '[-] Script [%s] not exist. Use [--show] to view all available script in ./scripts/' % args.script_name
             outputscreen.error(msg)
             sys.exit()
     # outputscreen.info(conf.Module_name) # test.py
     # outputscreen.info(conf.Module_file_path) #  D:\saucerframe\scripts\test.py
 
-    # 接下来加载module到conf.module_obj
-    msg = 'Load custom script: %s' % conf.module_name
+    # loader POC module to conf.module_obj
+    msg = '[+] Load custom script: %s' % conf.module_name
     outputscreen.success(msg)
 
     fp, pathname, description = imp.find_module(os.path.splitext(conf.module_name)[0], [paths.SCRIPT_PATH])
-    '''
-    >>> fp, pathname, description = imp.find_module('test',['D:\saucerframe\scripts'])
-    >>> fp
-        _io.TextIOWrapper name='D:\\saucerframe\\scripts\\test.py' mode='r' encoding='utf-8'>
-    >>> pathname
-        'D:\\saucerframe\\scripts\\test.py'
-    >>> description
-        ('.py', 'r', 1)
-    '''
     try:
-        conf.module_obj = imp.load_module("_", fp, pathname, description) # <module '_' from 'D:\\saucerframe\\scripts\\test.py'>
-        for each in ESSENTIAL_MODULE_METHODS: # each ='poc'
-            if not hasattr(conf.module_obj, each): # module_obj dot hasattr 'poc' module
-                errorMsg = "Can't find essential method:'%s' in current script，Please modify your script." % each
-                outputscreen.error(errorMsg)
+        conf.module_obj = imp.load_module("_", fp, pathname, description) 
+        for each in ESSENTIAL_MODULE_METHODS: 
+            if not hasattr(conf.module_obj, each): 
+                msg = "[-] Can't find essential method:'%s' in current script，Please modify your script." % each
+                outputscreen.error(msg)
                 sys.exit(0)
     except ImportError as e:
-        errorMsg = "Your current scipt [%s.py] caused this exception\n%s\n%s" \
+        msg = "[-] Your current script [%s.py] caused this exception\n%s\n%s" \
                    % (_name, '[Error Msg]: ' + str(e), 'Maybe you can download this module from pip or easy_install')
-        outputscreen.error(errorMsg)
+        outputscreen.error(msg)
         sys.exit(0)
 
 def TargetRegister(args):
-    infoMsg = 'Initialize targets...'
-    outputscreen.success(infoMsg)
+    msg = '[+] Initialize targets...'
+    outputscreen.success(msg)
     
-    # 设置一下api limit
+    # init target queue
     conf.target = queue.Queue()
 
-    # 单个目标>queue
+    # single target to queue
     if args.target_single:
         conf.target.put(args.target_single)
 
-    # 文件目标  e.g. D:/target.txt
+    # file target to queue
     elif args.target_file:
         if not os.path.isfile(args.target_file):
-            msg = 'TargetFile not found: %s' % args.target_file
+            msg = '[-] TargetFile not found: %s' % args.target_file
             outputscreen.error(msg)
             sys.exit()
 
@@ -140,18 +132,19 @@ def TargetRegister(args):
             targets = f.readlines()
             for target in targets:
                 conf.target.put(target.strip('\n'))
-    # 随机生成
+
+    # range of ip target to queue .e.g. 192.168.1.1-192.168.1.100
     elif args.target_range:
         try:
             lists = gen_ip(args.target_range)
             if (len(lists))>100000:
-                warnmsg = "Loading %d targets, Maybe it's too much, continue? [y/N]" % (len(lists))
+                warnmsg = "[*] Loading %d targets, Maybe it's too much, continue? [y/N]" % (len(lists))
                 outputscreen.warning(warnmsg)
                 flag =input()
                 if a in ('Y', 'y', 'yes', 'YES','Yes'):
                     pass
                 else:
-                    msg = 'User quit!'
+                    msg = '[-] User quit!'
                     outputscreen.warning(msg)
                     sys.exit()
             # save to conf
@@ -182,7 +175,7 @@ def TargetRegister(args):
             sys.exit()
             
     else:
-        # 如果都不是则就是调用了api,首先设置一下limit
+        # set search limit of api
         if args.api_limit <= 0:
             errormsg = 'Invalid input in [-limit] (can not be negative number)'
             outputscreen.error(errormsg)
@@ -199,12 +192,13 @@ def TargetRegister(args):
                 sys.exit()
         conf.limit = args.api_limit
         
+        # set search offset of api
         conf.offset = args.api_offset
 
         if args.zoomeye_dork:
-            # 如果是钟馗之眼还需要设置search_type
+            # verify search_type for zoomeye
             if args.search_type not in ['web', 'host']:
-                msg = 'Invalid value in [--search-type], show usage with [-h]'
+                msg = '[-] Invalid value in [--search-type], show usage with [-h]'
                 outputscreen.error(msg)
                 sys.exit()
             conf.search_type = args.search_type
@@ -221,33 +215,28 @@ def TargetRegister(args):
             conf.google_proxy = args.google_proxy
             handle_google(query=args.google_dork, limit = conf.limit, offset = conf.offset)
 
+    # verify targets number
     if conf.target.qsize() == 0:
         errormsg = msg = 'No targets found\nPlease load targets with [-iU|-iF|-iR|-iN] or use API with [-aZ|-aS|-aG|-aF]'
         outputscreen.error(errormsg)
         sys.exit()
 
-    # while conf.target.qsize() >0:
-    #     a = conf.target.get()
-    #     print(a,end='  ')
-            
-
 def Output(args):
     if args.no_output and args.output_path:
-        msg = 'Cannot use [-oF] and [-o] together, please read the usage with [-h].'
+        msg = '[-] Cannot use [-oF] and [-o] together, please read the usage with [-h].'
         outputscreen.error(msg)
         sys.exit()
 
     conf.no_output = args.no_output 
     
     if not args.no_output:
-        # not define output_path, default named by time
+        # if not define output, named it by time
         if not args.output_path:
             filename= time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) +'.txt'
-            #msg = 'not define output_filename, default named by time(%s)' % output_filename
-            #outputscreen.warning(msg)
-        
-        conf.output_path = os.path.join(paths.OUTPUT_PATH, filename)
 
+        conf.output_path = os.path.join(paths.OUTPUT_PATH, filename)
+        msg = '[+] Output: %s' % conf.output_path
+        outputscreen.success(msg)
 
 
 
