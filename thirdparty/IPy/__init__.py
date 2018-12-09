b6315f8c0a3a3faefa11fb0a32ae912625703dfe
saucerman <gyq0929@gmail.com>
2018-12-10 00:49:27 +0800

    deleted:    thirdparty/IPy/IPy.py 	deleted:    thirdparty/IPy/__init__.py 	deleted:    thirdparty/__init__.py

--- thirdparty/IPy/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (c) saucerman (https://xiaogeng.top)
-See the file 'LICENSE' for copying permission
-"""
--- thirdparty/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# project = https://github.com/saucer-man/saucerframe
-# author = saucerman
-pass
