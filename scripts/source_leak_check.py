adf5b7cfcbe388c7a69efa7bc3fb8ffeee137165
saucerman <gyq0929@gmail.com>
2019-02-25 18:16:51 +0800

    add copyright

--- scripts/source_leak_check.py
+++ scripts/source_leak_check.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) saucerman (https://xiaogeng.top)
+See the file 'LICENSE' for copying permission
+"""
+
+
 from lib.core.data import paths
 import requests
 
