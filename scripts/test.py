3f11e4c9954bc5badd7aeefc7b9b0e6e6b313a3e
saucerman <gyq0929@gmail.com>
2019-02-25 18:17:07 +0800

    git copyright

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# project = https://github.com/Xyntax/POC-T
-# author = i@cdxy.me
+
+"""
+Copyright (c) saucerman (https://xiaogeng.top)
+See the file 'LICENSE' for copying permission
+"""
 
 """
 测试用例
