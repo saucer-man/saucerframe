79989925e75d984917630f23bd093ab4993b94d1
saucerman <gyq0929@gmail.com>
2019-02-26 20:18:46 +0800

    modified banner

--- lib/core/setting.py
+++ lib/core/setting.py
@@ -7,16 +7,16 @@ See the file 'LICENSE' for copying permission
 """
 
 BANNER = r"""
- _______________
- Saucerframe
- By saucerman
- Email:w502325@qq.com
- ---------------
-        \ ^ __ ^
-         \（oo）\ _______
-            （__）\）\ / \
-                || ---- w |
-                || ||
+                                 __                          
+                                / _|                         
+  ___  __ _ _   _  ___ ___ _ __| |_ _ __ __ _ _ __ ___   ___ 
+ / __|/ _` | | | |/ __/ _ \ '__|  _| '__/ _` | '_ ` _ \ / _ \
+ \__ \ (_| | |_| | (_|  __/ |  | | | | | (_| | | | | | |  __/
+ |___/\__,_|\__,_|\___\___|_|  |_| |_|  \__,_|_| |_| |_|\___|
+                    ___________________
+                    By saucerman
+                    Email:w502325@qq.com
+                    -------------------                                                                                                                   
 """
 
 # essential methods/functions in custom scripts/PoC (such as function poc())
