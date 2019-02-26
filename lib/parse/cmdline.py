4cb173106fa571f7eef492d5e91c942d07abe161
saucerman <gyq0929@gmail.com>
2019-02-26 20:21:26 +0800

    add coroutine way

--- lib/parse/cmdline.py
+++ lib/parse/cmdline.py
@@ -13,12 +13,16 @@ def cmdLineParser():
     """
     This function parses the command line parameters and arguments
     """
-    parser = argparse.ArgumentParser(usage='python saucerframe-T.py -s bingc -aZ "port:8080"')
+    parser = argparse.ArgumentParser(usage='python3 saucerframe.py -s thinkphp_rce -aS "thinkphp"')
     
     # engine
-    engine = parser.add_argument_group("Engine", "Decide the threads of engine")
-    engine.add_argument("-t","--thread",  dest="thread_num", type=int, default=10,
-                        help="num of threads, 10 by default")
+    engine = parser.add_argument_group("Engine", "Decide the working way of engine")
+    engine.add_argument('-eT', dest="engine_thread", default=False, action='store_true',
+                        help='Multi-Threaded engine (default choice)')
+    engine.add_argument('-eG', dest="engine_gevent", default=False, action='store_true',
+                        help='Gevent engine (single-threaded with asynchronous)')
+    engine.add_argument("-t","--thread",  dest="thread_num", type=int, default=30,
+                        help="num of threads, default 30")
     
     # script
     script = parser.add_argument_group("Script", "Choice script you want to use")
@@ -40,15 +44,15 @@ def cmdLineParser():
     # api
     api = parser.add_argument_group('API')
     api.add_argument('-aZ', '--zoomeye', metavar='DORK', dest="zoomeye_dork", type=str, default='',
-                     help='ZoomEye dork (e.g. "zabbix port:8080")') # 钟馗之眼
+                     help='ZoomEye dork (e.g. "zabbix port:8080")') 
     api.add_argument('-aS', '--shodan', metavar='DORK', dest="shodan_dork", type=str, default='',
-                     help='Shodan dork.') # shodan
+                     help='Shodan dork.') 
     api.add_argument('-aG', '--google', metavar='DORK', dest="google_dork", type=str, default='',
-                     help='Google dork (e.g. "inurl:admin.php")') # google搜索 
+                     help='Google dork (e.g. "inurl:admin.php")') 
     api.add_argument('-aF', '--fofa', metavar='DORK', dest="fofa_dork", type=str, default='',
-                     help='FoFa dork (e.g. "banner=users && protocol=ftp")') # fofa
+                     help='FoFa dork (e.g. "banner=users && protocol=ftp")') 
     api.add_argument('--limit', metavar='NUM', dest="api_limit", type=int, default=50,
-                     help='Maximum searching results (default:50)') # 搜索数目
+                     help='Maximum searching results (default:50)') 
     api.add_argument('--offset', metavar='OFFSET', dest="api_offset", type=int, default=0,
                      help="Search offset to begin getting results from (default:0)")
     api.add_argument('--search-type', metavar='TYPE', dest="search_type", action="store", default='host',
