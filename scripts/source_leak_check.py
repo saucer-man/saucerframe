4b11bd2cbf6d3505f4e615bccf5c275455274156
saucerman <gyq0929@gmail.com>
2019-05-08 14:22:57 +0800

    :white_check_mark:modify logic

--- scripts/source_leak_check.py
+++ scripts/source_leak_check.py
@@ -13,6 +13,7 @@ from gevent.queue import Queue
 from lib.core.data import paths
 import requests
 from urllib.parse import urlparse
+from plugin.random_ua import get_random_ua
 
 def poc(url):
     # url = "http://www.example.org/default.html?ct=32&op=92&item=98"
@@ -35,7 +36,7 @@ def poc(url):
 
 def bak_scan(url, payloads, result):
     headers = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+        "User-Agent":get_random_ua()
         }
     while not payloads.empty():
         payload = payloads.get()
@@ -45,7 +46,10 @@ def bak_scan(url, payloads, result):
             # 如果是备份文件则不需要下载，只需要head方法获取头部信息即可，否则文件较大会浪费大量的时间
             if 'zip' in payload or 'rar' in payload or 'gz' in payload or 'sql' in payload:
                 req = requests.head(vulnurl, headers=headers, timeout=5, allow_redirects=False, verify=False)
-
+                # 404页面 'Content-Type': 'application/octet-stream',
+                # zip 'application/x-zip-compressed' 'application/zip'
+                # rar 'application/octet-stream'  'application/x-rar-compressed'
+                # 采用Content-Type过滤，还是有一定误报
                 if req.status_code == 200:
                     if 'html' not in req.headers['Content-Type'] and 'image' not in req.headers['Content-Type']:
                         flag = 1
