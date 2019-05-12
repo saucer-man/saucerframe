7664cad184261d330f990f28e40b3350420d1f06
saucerman <gyq0929@gmail.com>
2019-05-12 21:10:17 +0800

    modify library functions and code logic

--- lib/core/common.py
+++ lib/core/common.py
@@ -6,79 +6,90 @@ Copyright (c) saucerman (https://saucer-man.com)
 See the file 'LICENSE' for copying permission
 """
 
-import sys
 import os.path
 from lib.core.setting import BANNER
-from lib.core.enums import COLOR
-from lib.core.data import paths, conf, cmdLineOptions
-from thirdlib.colorama import init, Fore, Back, Style
+from lib.core.data import paths
+from thirdlib.colorama import init, Fore, Style
 
 init(autoreset=True)
-class Outputscreen:
-    def info(self, s):
+
+
+class ColorPrint:
+
+    @staticmethod
+    def white(s):
         print(Style.BRIGHT+Fore.WHITE + str(s) + Fore.RESET+Style.RESET_ALL)
 
-    def success(self, s):
+    @staticmethod
+    def green(s):
         print(Style.BRIGHT+Fore.GREEN + str(s) + Fore.RESET+Style.RESET_ALL)
 
-    def warning(self, s):
+    @staticmethod
+    def cyan(s):
         print(Style.BRIGHT+Fore.CYAN + str(s) + Fore.RESET+Style.RESET_ALL)
 
-    def error(self, s):
+    @staticmethod
+    def red(s):
         print(Style.BRIGHT+Fore.RED + str(s) + Fore.RESET+Style.RESET_ALL)
-    
-    def blue(self, s): # for banner
+
+    @staticmethod
+    def blue(s):
         print(Style.BRIGHT+Fore.BLUE + str(s) + Fore.RESET+Style.RESET_ALL)
 
-outputscreen=Outputscreen()
+    @staticmethod
+    def yellow(s):
+        print(Style.BRIGHT+Fore.YELLOW + str(s) + Fore.RESET+Style.RESET_ALL)
 
-def setpaths():
+
+colorprint = ColorPrint()
+
+
+def set_paths(root_path):
     """
     Sets absolute paths for project directories and files
     """
-    root_path = paths.ROOT_PATH # 根目录
-    paths.DATA_PATH = os.path.join(root_path, "data") # datapath
-    paths.SCRIPT_PATH = os.path.join(root_path, "scripts")
-    paths.OUTPUT_PATH = os.path.join(root_path, "output")
-    paths.CONFIG_PATH = os.path.join(root_path, "saucerframe.conf")
+    paths.ROOT_PATH = root_path # root path
+    paths.DATA_PATH = os.path.join(paths.ROOT_PATH, "data")
+    paths.SCRIPT_PATH = os.path.join(paths.ROOT_PATH, "scripts")
+    paths.PLUGIN_PATH = os.path.join(paths.ROOT_PATH, "plugin")
+    paths.OUTPUT_PATH = os.path.join(paths.ROOT_PATH, "output")
+    paths.CONFIG_PATH = os.path.join(paths.ROOT_PATH, "saucerframe.conf")
     if not os.path.exists(paths.SCRIPT_PATH):
+        err_msg = "script file missing,it may cause some issues."
+        colorprint.red(err_msg)
         os.mkdir(paths.SCRIPT_PATH)
+    if not os.path.exists(paths.PLUGIN_PATH):
+        err_msg = "plugin file missing,it may cause some issues."
+        colorprint.red(err_msg)
+        os.mkdir(paths.PLUGIN_PATH)
     if not os.path.exists(paths.OUTPUT_PATH):
         os.mkdir(paths.OUTPUT_PATH)
     if not os.path.exists(paths.DATA_PATH):
         os.mkdir(paths.DATA_PATH)
 
-    # paths.WEAK_PASS = os.path.join(paths.DATA_PATH, "pass100.txt")
-    # paths.LARGE_WEAK_PASS = os.path.join(paths.DATA_PATH, "pass1000.txt")
-    # paths.UA_LIST_PATH = os.path.join(paths.DATA_PATH, "user-agents.txt")
-
     if os.path.isfile(paths.CONFIG_PATH):
         pass
     else:
-        msg = 'Config files missing, it may cause an issue.\n'
-        outputscreen.error(msg)
-        sys.exit(0)
-    
-    #print(root_path,paths.DATA_PATH,paths.SCRIPT_PATH,paths.OUTPUT_PATH,paths.CONFIG_PATH)
-    #print(paths.WEAK_PASS,paths.LARGE_WEAK_PASS,paths.UA_LIST_PATH)
+        err_msg = 'Config files missing, it may cause some issues.\n'
+        colorprint.red(err_msg)
+
 
 def banner():
-    outputscreen.blue(BANNER)
+    colorprint.blue(BANNER)
 
-# 将'192.168.1.1 -192.168.1.100'分解成ip地址列表
+
 def gen_ip(ip_range):
     '''
     print (gen_ip('192.18.1.1-192.168.1.3'))
-    ['192.168.1.1', '192.168.1.2', '192.168.1.3']
+    ==> ['192.168.1.1', '192.168.1.2', '192.168.1.3']
+    from https://segmentfault.com/a/1190000010324211
     '''
-    # from https://segmentfault.com/a/1190000010324211
-    def num2ip (num):
+    def num2ip(num):
         return '%s.%s.%s.%s' % ((num >> 24) & 0xff, (num >> 16) & 0xff, (num >> 8) & 0xff, (num & 0xff))
     
     def ip2num(ip):
         ips = [int(x) for x in ip.split('.')]
-        return ips[0]<< 24 | ips[1]<< 16 | ips[2] << 8 | ips[3]
+        return ips[0] << 24 | ips[1] << 16 | ips[2] << 8 | ips[3]
 
-    start ,end = [ip2num(x) for x in ip_range.split('-')]
-    return [num2ip(num) for num in range(start,end+1) if num & 0xff]
-    
+    start, end = [ip2num(x) for x in ip_range.split('-')]
+    return [num2ip(num) for num in range(start, end+1) if num & 0xff]
