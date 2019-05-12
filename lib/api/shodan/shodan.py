c4ba5cac57fb2343b28d43653ffcaa6e781b4735
saucerman <gyq0929@gmail.com>
2019-05-12 21:08:07 +0800

    specification code

--- lib/api/shodan/shodan.py
+++ lib/api/shodan/shodan.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 Copyright (c) saucerman (https://saucer-man.com)
@@ -7,10 +7,11 @@ See the file 'LICENSE' for copying permission
 """
 
 import sys
-from lib.core.data import paths,conf
-from lib.core.common import outputscreen
+from lib.core.data import paths, conf
+from lib.core.common import colorprint
 from lib.utils.config import ConfigFileParser
 from shodan import Shodan, APIError
+
 
 class ShodanBase:
     def __init__(self, query, limit, offset):
@@ -21,27 +22,27 @@ class ShodanBase:
 
     def login(self):
         msg = '[+] Trying to login with credentials in config file: %s.' % paths.CONFIG_PATH
-        outputscreen.success(msg)
+        colorprint.green(msg)
         self.api_key = ConfigFileParser().shodan_apikey()
 
         if not self.api_key:
             msg = '[*] Automatic authorization failed.'
-            outputscreen.warning(msg)
+            colorprint.cyan(msg)
             msg = '[*] Please input your Shodan API Key (https://account.shodan.io/).'
-            outputscreen.warning(msg)
+            colorprint.cyan(msg)
             self.api_key = input('[*] API KEY > ').strip()
 
     def account_info(self):
         try:
             if not self.api_key:
-                outputscreen.error("[-] Shodan api cant not be Null")
+                colorprint.red("[-] Shodan api cant not be Null")
                 sys.exit()
             api = Shodan(self.api_key)
             account_info = api.info()
             msg = "[+] Available Shodan query credits: %d" % account_info.get('query_credits')
-            outputscreen.success(msg)
+            colorprint.green(msg)
         except APIError as e:
-            outputscreen.error(e)
+            colorprint.red(e)
             sys.exit()
         return True
 
@@ -50,7 +51,7 @@ class ShodanBase:
             api = Shodan(self.api_key)
             result = api.search(query=self.query, offset=self.offset, limit=self.limit)
         except APIError as e:
-            outputscreen.error(e)
+            colorprint.red(e)
             sys.exit()
 
         if 'matches' in result:
