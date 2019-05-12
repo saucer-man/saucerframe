0f580de82f98e842d36a8c00dbc263692313e232
saucerman <gyq0929@gmail.com>
2019-05-12 21:20:00 +0800

    modify banner

--- lib/core/setting.py
+++ lib/core/setting.py
@@ -5,19 +5,21 @@
 Copyright (c) saucerman (https://saucer-man.com)
 See the file 'LICENSE' for copying permission
 """
+import sys
+import os
 
+GIT_REPOSITORY = "https://github.com/saucer-man/saucerframe"
+WEBSITE = "https://saucer-man.com"
 BANNER = r"""
-                                 __                          
-                                / _|                         
-  ___  __ _ _   _  ___ ___ _ __| |_ _ __ __ _ _ __ ___   ___ 
- / __|/ _` | | | |/ __/ _ \ '__|  _| '__/ _` | '_ ` _ \ / _ \
- \__ \ (_| | |_| | (_|  __/ |  | | | | | (_| | | | | | |  __/
- |___/\__,_|\__,_|\___\___|_|  |_| |_|  \__,_|_| |_| |_|\___|
-                    ___________________
-                    By saucerman
-                    Email:w502325@qq.com
-                    -------------------                                                                                                                   
-"""
+                               __  {website}                         
+                              / |                         
+___  __ _ _   _  ___ ___ _ ___| | _ __ __ _ _ __ ___   ___ 
+/ __|/ _` | | | |/ __/ _ \ '__|  _| '__/ _` | '_ ` _ \ / _ \
+\__ \ (_| | |_| | (_|  __/ |  | | | | | (_| | | | | | |  __/
+|___/\__,_|\__,_|\___\___|_|  |_| |_|  \__,_|_| |_| |_|\___|                                                                                                
+""".format(website=WEBSITE)
 
 # essential methods/functions in custom scripts/PoC (such as function poc())
-ESSENTIAL_MODULE_METHODS = ['poc']
+ESSENTIAL_MODULE_METHODS = 'poc'
+
+IS_WIN = True if (sys.platform in ["win32", "cygwin"] or os.name == "nt") else False
