e949ced10e2b1830e8e2ef3a562a6a3d99bc5f87
unknown <yanqing.geng@yitu-inc.com>
2019-07-25 16:48:08 +0800

    rewrite censys api

--- lib/api/censys/censys.py
+++ lib/api/censys/censys.py
@@ -7,7 +7,7 @@ Copyright (c) saucerman (https://saucer-man.com)
 See the file 'LICENSE' for copying permission
 """
 
-import requests
+from lib.core.Request import request
 import sys
 import time
 import json
@@ -23,7 +23,7 @@ def can_auto_login():
         msg = '[+] Trying to login with credentials in config file: %s.' % paths.CONFIG_PATH
         colorprint.green(msg)
         try:
-            res = requests.get(API_URL + "/data", auth=(UID, SECRET), timeout = 10)
+            res = request.get(API_URL + "/data", auth=(UID, SECRET), timeout = 10)
             if res.status_code != 200:
                 raise SystemExit
             else:
@@ -45,16 +45,16 @@ def get_ip(query, page):
     }
 
     try:
-        res = requests.post(API_URL + "/search/ipv4", data=json.dumps(data), auth=(UID, SECRET))
+        res = request.post(API_URL + "/search/ipv4", data=json.dumps(data), auth=(UID, SECRET))
         results = res.json()
+        
         if res.status_code != 200:
             colorprint.red("error occurred: %s" % results["error"])
             sys.exit(1)
 
         # add result in some specific form
         for result in results["results"]:
-            for i in result["protocols"]:
-                conf.target.put(i.split('/')[1] + "://" + result["ip"] + ':' + i.split('/')[0])
+            conf.target.put(result["ip"])
 
     except Exception as e:
         colorprint.red(e)
@@ -66,27 +66,24 @@ def handle_censys(query, limit, offset):
     UID = ConfigFileParser().censys_UID()
     SECRET = ConfigFileParser().censys_SECRET()
     if not can_auto_login():
-        err_msg = '[-] Automatic authorization failed.\n\
-                   [*] Please input your Shodan API Key (https://account.shodan.io/).'
+        err_msg = '[-] Automatic authorization failed.\n[*] Please input your censys API Key (https://censys.io/account/api).'
         colorprint.cyan(err_msg)
         UID = input('[*] UID > ').strip()
-        SECRET = input('[*] UID > ').strip()
-        if not can_auto_login(UID, SECRET):
+        SECRET = input('[*] SECRET > ').strip()
+        if not can_auto_login():
             err_msg = "[-] authorization failed"
             colorprint.red(err_msg)
-        else:
-            pass
-    else:
+            sys.exit()
 
-        page_start = int(offset/160) + 1
-        page_stop = page_start + int(limit/160) + 1
+    page_start = int(offset/100) + 1
+    page_stop = page_start + int(limit/100) + 1
 
-        for page in range(page_start, page_stop):
-            ip_list = get_ip(query, page)
+    for page in range(page_start, page_stop):
+        get_ip(query, page)
 
-            # the last loop dont need sleep
-            if page < page_stop - 1:
-                time.sleep(3)
+        # the last loop dont need sleep
+        if page < page_stop - 1:
+            time.sleep(3)
 
 
 
