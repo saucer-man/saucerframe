4e269611209a294bdeb630ee791f261c1ee71f8b
unknown <yanqing.geng@yitu-inc.com>
2019-08-10 13:21:43 +0800

    delete poc name add version

--- lib/core/setting.py
+++ lib/core/setting.py
@@ -19,7 +19,6 @@ ___  __ _ _   _  ___ ___ _ ___| | _ __ __ _ _ __ ___   ___
 |___/\__,_|\__,_|\___\___|_|  |_| |_|  \__,_|_| |_| |_|\___|                                                                                                
 """.format(website=WEBSITE)
 
-# essential methods/functions in custom scripts/PoC (such as function poc())
-ESSENTIAL_MODULE_METHODS = 'poc'
+version = 1.0
 
 IS_WIN = True if (sys.platform in ["win32", "cygwin"] or os.name == "nt") else False
